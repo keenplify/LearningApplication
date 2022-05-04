d1a76cc19bb13c8a05a24ac3e43a301db1d7a58e
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2022-05-05 00:47:08 +0800

    Connection + Nuget packages

---
+++ Helpers/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Data.Sql;
+using Microsoft.Data.SqlClient;
+
+namespace LearningApplication.Helpers
+{
+    public class Database
+    {
+        public static SqlConnection Connect()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection
+                {
+                    ConnectionString =
+                    "server=" + Helpers.Connection.HOSTNAME +
+                    ";uid=" + Helpers.Connection.USERNAME +
+                    ";pwd=" + Helpers.Connection.PASSWORD +
+                    ";database=" + Helpers.Connection.DATABASE
+                };
+                Connection.Open();
+
+                return Connection;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
